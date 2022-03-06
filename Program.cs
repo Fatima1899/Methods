47444ed981afbb16d8a16c5393c277019897d0d7
Fatima1899 <99797429+Fatima1899@users.noreply.github.com>
2022-03-06 11:58:51 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Musbet_eded
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task.2
+            //string word = ("Fatima");
+            //int count = 0;
+            //foreach (char item in word)
+            //{
+            //    if (item == 'a')
+            //    {
+            //        count++;
+            //    }
+            //}
+
+
+            //{
+
+            //}
+
+            //{
+            //    Console.WriteLine("a-nin sayi {0}", +count);
+            //}
+            #endregion
+            #region Task.1
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int count = 0;
+            
+            //    while (number > 0)
+            //    {
+            //        number=number / 10;
+            //        count++;
+
+            //    }
+
+            //    Console.WriteLine("mertebelerin sayi:" +count );
+           
+
+            #endregion  
+        }
+
+
+
+    }
+}
