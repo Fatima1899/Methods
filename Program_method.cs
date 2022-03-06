5330012eef9979806d87b4b46b0018054fa34a8f
Fatima1899 <99797429+Fatima1899@users.noreply.github.com>
2022-03-06 11:53:50 -0800

    Add files via upload

---
+++ Program_method.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Çevrə
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            //area(4);
+            //area(5, 6);
+            //area(10, 2,5,10);
+            
+            //int Result = Division(8, 2);
+            //Console.WriteLine(Result);
+
+            //int sum = Cəm (1, 10, 12, 13);
+            //Console.WriteLine(sum);
+
+            //Console.WriteLine(Repeat("Ha!",6));
+        }
+        #region Task.4
+        //static void area(int r)
+        //{
+        //    int p = 3;
+        //    float s = 0;
+        //    s = p * r * r;
+        //    Console.WriteLine(s);
+        //}
+        //static void area(int a,int b)
+        //{
+            
+        //    float s = 0;
+        //    s = a*b;
+        //    Console.WriteLine(s);
+        //}
+        //static void area(int r, int a ,int b, int c)
+        //{
+        //    float s = 0;
+        //    float  p = (a + b + c)/2;
+        //    s = p * r;
+        //    Console.WriteLine(s);
+        //}
+        //static void Sahə(string word)
+        //{
+        //    Console.WriteLine($"S = a*b {word}");
+        //}
+        //static void Sahə(string word)
+        //{
+        //    Console.WriteLine($"S=p*r; p=(a+b+c)/2 {word}");
+        //}
+        #endregion
+        #region Task.1
+        //static int Division(int num1,int num2)
+        //{
+        //    return num1 / num2;
+        //}
+        #endregion
+        #region Task.3
+        //static int Cəm(params int []arr)
+        //{
+        //    int sum = 0;
+        // for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        sum += arr[i];
+        //    }
+        //    return sum;
+        //}
+        #endregion
+        #region Task.2
+        //static string Repeat (string word, int count)
+        //{
+        //    string word2="";
+
+
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        word2 += word;
+        //    }
+        //    return word2 ;
+        //}
+
+
+
+
+
+        #endregion
+    }
+}
